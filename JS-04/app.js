f19e825bd610bbfcf79c090b841bb1e8fa371a33
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-04-01 16:39:36 +0400

    JS-04

---
+++ JS-04/app.js
@@ -0,0 +1,16 @@
+//Task-03
+
+//Task-04
+// let nums = prompt(" 5 Eded daxil edin.");
+// let numArr = nums.split(",");
+// let minNum = +nums[0];
+// for (let i = 1; i < numArr.length; i++) {
+//   if (+numArr[i] < minNum) {
+//     minNum = numArr[i];
+//   }
+// }
+// console.log(minNum);
+//Task-05
+// let str = "32, 31, 34, 36, 31";
+// let newStr = str.split(",");
+// console.log(newStr.join(";"));
